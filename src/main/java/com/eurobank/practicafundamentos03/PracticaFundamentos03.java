18bc4f7d95c258fb1769237d6dd0f3fcf26dcad4
MZSM98 <marcoszenon98@gmail.com>
2025-09-23 13:46:35 -0600

    creación de repositorio

---
+++ src/main/java/com/eurobank/practicafundamentos03/PracticaFundamentos03.java
@@ -0,0 +1,13 @@
+
+package com.eurobank.practicafundamentos03;
+
+/**
+ *
+ * @author User
+ */
+public class PracticaFundamentos03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
